cf6313a083b9b795748e9db8ed370a4234f04a7c
shpetimshala <shpetim.sha@gmail.com>
2020-09-18 13:56:12 +0200

    first commit

---
+++ api/src/index.js
@@ -0,0 +1,32 @@
+// @flow
+import serverless from 'serverless-http';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import blogRoutes from '@routes/blog-routes';
+import articleRoutes from '@routes/article-routes';
+// ROUTE_IMPORT_PLACEHOLDER
+
+export const app = express();
+app.use(cors());
+app.use(bodyParser.json({ limit: '10mb', extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/blogs', blogRoutes);
+app.use('/articles', articleRoutes);
+// ROUTE_USE_PLACEHOLDER
+
+// catch 404 and forward to error handler
+app.use((req, _, next) => {
+	console.error('Path not found:', req.originalUrl);
+	next(new Error('Not Found'));
+});
+
+// error handler
+app.use((err, _, res) => {
+	console.error(err);
+	const errorStatus = 500;
+	res.status(err.statusCode || errorStatus).json(err);
+});
+
+export const handler = serverless(app);
